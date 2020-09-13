7d86cbd4bdc7cfe303cb87bbe5cbe59848b128f1
efulp <31475571+efulp@users.noreply.github.com>
2020-09-13 19:45:40 -0400

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,7 +1,7 @@
 /**
  * This program reads a text file and prints the top 5 most frequent letters (upper or lower case)
  * and the top 5 most frequent digits that are in the file. If the file has less than 5 unique letters
- * and/or digits the top list is ajusted accordingly. The file name is provided using command line 
+ * and/or digits the top list is adjusted accordingly. The file name is provided using command line 
  * arguments. If the file name is not provided or the file is not readable, the program will exit
  * and provide an error message.
  *
